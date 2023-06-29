fb4b5a4ca8b335606fbddc147e42ce0c115262f8
Omkar Borker <omkarborker15@gmail.com>
2023-06-29 17:26:17 +0530

    Updates

---
+++ precache-manifest.13781e609f08d6f40b95a57de2ebaca6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "425b13bbe543ff406ae1",
+    "url": "/static/css/main.ed4ef359.chunk.css"
+  },
+  {
+    "revision": "425b13bbe543ff406ae1",
+    "url": "/static/js/main.202fe438.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "0b81905df4f5a3d76a11",
+    "url": "/static/js/2.6fbe69d9.chunk.js"
+  },
+  {
+    "revision": "13bf794a3596d6c20bb5bbbf3740bb38",
+    "url": "/index.html"
+  }
+];
